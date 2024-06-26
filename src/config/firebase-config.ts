28deefcaa50cc1058c5f63df89edf0e6a6bab46e
robMolloy <robmolloy@hotmail.co.uk>
2024-06-26 11:42:31 +0100

    auth setup

--- src/config/firebase-config.ts
+++ src/config/firebase-config.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { connectAuthEmulator, getAuth } from "firebase/auth";
 import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -20,7 +20,11 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth();
+export const initAuth = getAuth();
 const initDb = getFirestore(app);
+
 connectFirestoreEmulator(initDb, "127.0.0.1", 8080);
+connectAuthEmulator(initAuth, "http://127.0.0.1:9099");
+
+export const auth = initAuth;
 export const db = initDb;
